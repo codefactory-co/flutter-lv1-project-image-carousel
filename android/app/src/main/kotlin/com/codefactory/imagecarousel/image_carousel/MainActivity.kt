c2973465f989043a74c9ad6b2caace5e94790810
Ji Ho Choi <jihochoi@Jiui-MacBookPro.local>
2022-02-21 13:26:58 +0900

    done

---
+++ android/app/src/main/kotlin/com/codefactory/imagecarousel/image_carousel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codefactory.imagecarousel.image_carousel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:image_carousel/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
